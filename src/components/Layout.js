c1a0a19a959e949025bd1d915fea21566434a6b4
Andre <vtavares.andre@gmail.com>
2021-02-15 12:32:14 -0300

    Next and Materia UI start Point

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Layout = ({ children }) => {
+	return (
+		<div className=''>
+			{/* <Nav /> */}
+			<div>{children}</div>
+
+			{/* <Footer /> */}
+		</div>
+	)
+}
+
+export default Layout
